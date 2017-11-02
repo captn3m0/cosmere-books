1401c1ad6569a8ec0f36647b03aab8e93ab49e5f
Nemo <me@captnemo.in>
2017-11-03 03:13:38 +0530

    Updates edgedancer reread (FIN)

--- edgedancer-reread.rb
+++ edgedancer-reread.rb
@@ -11,7 +11,14 @@ links = [
   '2017/08/24/edgedancer-reread-chapter-1/',
   '2017/08/31/edgedancer-reread-chapter-2/',
   '2017/09/07/edgedancer-reread-chapters-3-and-4/',
-  '2017/09/14/edgedancer-reread-chapters-5-and-6/'
+  '2017/09/14/edgedancer-reread-chapters-5-and-6/',
+  '2017/09/21/edgedancer-reread-chapters-7-and-8/',
+  '2017/09/28/edgedancer-reread-chapters-9-and-10/',
+  '2017/10/05/edgedancer-reread-chapters-11-and-12/',
+  '2017/10/12/edgedancer-reread-chapters-13-and-14/',
+  '2017/10/19/edgedancer-reread-chapters-15-and-16/',
+  '2017/10/26/edgedancer-reread-chapters-17-and-18/',
+  '2017/11/02/edgedancer-reread-chapters-19-and-20/',
 ]
 
 episode = 1

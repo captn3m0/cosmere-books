7287051ceee01baa1436326804275b5ff5394378
Nemo <me@captnemo.in>
2020-08-30 18:07:04 +0530

    Adds annotations for Rhythm of War

---
+++ row-annotations.rb
@@ -0,0 +1,41 @@
+$annotations = []
+
+$annotations[1] = <<EOT
+So, for a little commentary on these chapters, you might find it interesting that I plotted this opening sequence as if it were a climax section of a book. In the Stormlight novels, I generally limit myself to one viewpoint a chapter. This is to give a stronger identity to the chapters and characters--we usually get big chunks from a person's viewpoint (with chapters that average two or three times as long as chapters from something like Wax and Wayne or Skyward.) This gives each chapter a kind of short-story feel with their own arcs and themes.
+
+However, as I approach climactic sections of books, I bleed the viewpoints across one another, adding to the frantic feel of a building crescendo. Viewpoints alternate in quick succession, with bite-sized chunks, hooks and payoffs, like one might plot closer to what you'd see in a thriller novel. The goal here is to evoke quick scene changes, lots of twists and turns, and a general sense that viewpoints are piling up on top of one another to enhance the feeling of an impending climax.
+
+In a normal stormlight book, I generally start slow and build to such a climax near the end of part one. (Though I usually don't start the full viewpoint bleeds until the end of the book.) Here, I wanted to give the feeling that the year that passed had its own narrative arc, and some of those threads were culminating here. So we're beginning the book at the end of the "previous book" (imagining the in-between year as a "book."
+
+That led to some confusion and consternation among alpha and beta readers, since this isn't how a Stormlight book generally begins--but in this case, I decided I was all right with that feeling, as this truly was the tone I wanted starting out.
+EOT
+
+$annotations[2] = <<EOT
+Here's an annotation for these chapters! One of the most revised sequences of this book were these Shallan chapters--continuing through the entire novel. As I have said elsewhere, I originally designed Shallan's mental state to be a more fantastical look at something like Dissociative Identity Disorder. (Like the fantastical look at Schizophrenia I did with Stephen Leeds.)
+
+I was fascinated by how something like mental health challenges relating to identity would intersect with magic that let you quite literally become someone else. The original version of this was for a character I wrote in Dragonsteel--which I'll eventually release to the public like I've done with TWOK Prime.
+
+In this series, however, I've found myself leaning away from the fantastical elements more and more, and trying to lean into the real science and best mental health practices. This is because I've realized that having Shallan's ailment be completely fantastical was both irresponsible (in representation terms) and less realistic. Where I settled earlier in the series was in representing not someone with a fantastical disease, but someone with a very real disease--that is exacerbated by fantastical elements.
+
+Because of this, I listened very hard to my beta readers on Shallan, particularly those with specific experience in this area. In the original draft of these scenes, for example, Shallan wasn't shifting between the various alters of herself nearly as often--and with some feedback, I tweaked that, and found it not only worked better in a realism way, but it also read far, far better. It's simply more interesting to see Shallan's different aspects doing different things, thinking different ways.
+
+Some of the most satisfying moments in revisions come when you try something different, and find that it's what you wanted to do all along--but didn't quite know how to accomplish until a comment nudges you.
+EOT
+
+$annotations[4] = <<EOT
+Annotation time! So, one of the things I worry about (maybe too much) in an extended series like this is something I'll call Skelletor Syndrome. This is the problem that the protagonists need victories through the course of the series--the text will naturally build to important moments, and while there will be failures, there will also be victories.
+
+The more times an antagonist gets defeated, however, the less of a threat they become in the reader's mind. It's hard to justify to the reader that a villain is still a credible threat after they've been foiled time and time again. (Kylo Ren ran into this problem, for example, in the new Star Wars series.)
+
+Going into the Stormlight Archive, this is why I staggered the threats moving from non-supernatural antagonists (like Sadeas) toward increasingly dangerous threats. This isn't to say that someone like Ialai couldn't be a credible threat without powers. However, I still felt it best to move on from her as a representation of the antagonists in the earlier part of the series, pointing us toward larger (and more cosmere-aware) threats as the conflict of the books expands. I could easily have had an entire book with a major thread about toppling her little empire on the Shattered Plains, but that would have been too backward looking.
+
+So in this book, we're pointing away from the Sadeas/Amaram team toward Odium, some individual fused, and several of the cosmere-aware players (Thaidakar and Restares.) Don't worry if those names aren't clear to you on first read--they've been around for a while, but I haven't delved too much into who they are. This book will do so.
+EOT
+
+$annotations[5] = <<EOT
+Annotation for this chapter: Moash was one of the characters that was most difficult to get right for this book. There's a difficult balance to maintain with him, compounded by how difficult a line I'm walking with Kaladin in these chapters. I had to do several tone rewrites of this chapter after the Alpha read, to make it all work.
+
+Part of the trick was to convey just how exhausted Kaladin is, mentally while in his viewpoint--since he doesn't accept it himself. Then mix that with a Moash who, in part, does still want to be a good friend--but no longer is capable of reasoning in a conventional way. (And who won't acknowledge to himself that being right, proving that he made the right decisions, is actually far more important to him than his friendships ever were.)
+
+You'll get a Moash viewpoint in a future interlude, which should help explain where his mindset is these days. As for Kaladin, well, it's becoming more and more difficult for him to maintain the lie that everything is fine.
+EOT
--- row.rb
+++ row.rb
@@ -4,6 +4,7 @@ require 'date'
 require 'fileutils'
 require 'nokogiri'
 require_relative './methods'
+require_relative './row-annotations'
 FileUtils.mkdir_p('row')
 
 BASE = 'https://www.tor.com/2020/'
@@ -12,7 +13,9 @@ links = [
   '07/23/read-rhythm-of-war-by-brandon-sanderson-prologue-and-chapter-one/',
   '07/28/read-rhythm-of-war-by-brandon-sanderson-chapters-two-and-three/',
   '08/04/read-rhythm-of-war-by-brandon-sanderson-chapters-four-and-five/',
-  '08/11/read-rhythm-of-war-by-brandon-sanderson-chapter-six/'
+  '08/11/read-rhythm-of-war-by-brandon-sanderson-chapter-six/',
+  '08/18/read-rhythm-of-war-by-brandon-sanderson-chapter-seven/',
+  '08/25/read-rhythm-of-war-by-brandon-sanderson-chapter-eight/',
 ]
 
 # Automatically adds all recent chapters
@@ -59,11 +62,23 @@ html = ''
 
     e.remove if !start || ending
   end
-  html += page.inner_html
+  chapter_html = page.inner_html.sub(/<h1/, "<h1 id='chapter-#{i-1}'")
+  html += chapter_html
+  if $annotations[i-1]
+    html += "<p><a href='#annotation-#{i-1}'>Click here</a> to reach Brandon's annotations for this chapter.</p>"
+  end
   url = BASE + links[i - 1]
 end
 
-html += "<p>Next 3 chapters out on #{next_date}</p>"
+html += "<p>Next chapter out on #{next_date}</p>"
+
+$annotations.each_with_index do |a, i|
+  if a
+    html += "<h1 id='annotation-#{i}'>Annotations - " + links[i].split('/').last[40..] + "</h1>"
+    html += a.gsub(/(\r)?\n/, "<br>")
+    html += "<a href='#chapter-#{i+1}'>Click here</a> to go the next chapter."
+  end
+end
 
 File.open('books/row.html', 'w') { |file| file.write(html) }
 puts '[html] Generated HTML file'
